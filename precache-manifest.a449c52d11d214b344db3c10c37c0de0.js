058f806cc87ad9ba32fa7efadde901a15b235647
swiichy <boomheadshotmn@gmail.com>
2021-06-04 04:42:37 +0200

    Updates

---
+++ precache-manifest.a449c52d11d214b344db3c10c37c0de0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb75e47f352b9c67f849d5ae97b92f01",
+    "url": "/Heavens-r/index.html"
+  },
+  {
+    "revision": "c40731b44d4b2b6d2ee8",
+    "url": "/Heavens-r/static/css/main.0333e0d4.chunk.css"
+  },
+  {
+    "revision": "1272765d0e15e57d4c55",
+    "url": "/Heavens-r/static/js/2.e7c366c1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Heavens-r/static/js/2.e7c366c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40731b44d4b2b6d2ee8",
+    "url": "/Heavens-r/static/js/main.c24f7043.chunk.js"
+  },
+  {
+    "revision": "789aab582cf58ab50ac7",
+    "url": "/Heavens-r/static/js/runtime-main.1282d394.js"
+  },
+  {
+    "revision": "4b919f591de5ef203f0de0ae52898c4a",
+    "url": "/Heavens-r/static/media/bg_heavens.4b919f59.jpg"
+  },
+  {
+    "revision": "ded5333be526985f57295178379420ee",
+    "url": "/Heavens-r/static/media/bg_outils.ded5333b.jpg"
+  },
+  {
+    "revision": "2059b4916148e23fa4d7a3b96298ae84",
+    "url": "/Heavens-r/static/media/logo.2059b491.png"
+  }
+]);
