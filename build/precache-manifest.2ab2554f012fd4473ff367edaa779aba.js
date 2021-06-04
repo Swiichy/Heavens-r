e84fd1a2e7a09abe13af977dc5c32f72f808fc40
swiichy <boomheadshotmn@gmail.com>
2021-06-04 06:04:09 +0200

    .

--- build/precache-manifest.2ab2554f012fd4473ff367edaa779aba.js
+++ build/precache-manifest.2ab2554f012fd4473ff367edaa779aba.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb75e47f352b9c67f849d5ae97b92f01",
+    "revision": "2714591ade6196c1b5e98ead0259d691",
     "url": "/Heavens-r/index.html"
   },
   {
-    "revision": "c40731b44d4b2b6d2ee8",
+    "revision": "0c3ee78ab56f6d59b29d",
     "url": "/Heavens-r/static/css/main.0333e0d4.chunk.css"
   },
   {
-    "revision": "1272765d0e15e57d4c55",
-    "url": "/Heavens-r/static/js/2.e7c366c1.chunk.js"
+    "revision": "10a32ebfcd352b743d53",
+    "url": "/Heavens-r/static/js/2.7da4f40d.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/Heavens-r/static/js/2.e7c366c1.chunk.js.LICENSE.txt"
+    "url": "/Heavens-r/static/js/2.7da4f40d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c40731b44d4b2b6d2ee8",
-    "url": "/Heavens-r/static/js/main.c24f7043.chunk.js"
+    "revision": "0c3ee78ab56f6d59b29d",
+    "url": "/Heavens-r/static/js/main.27edc9cb.chunk.js"
   },
   {
     "revision": "789aab582cf58ab50ac7",
